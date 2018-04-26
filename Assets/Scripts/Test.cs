0acf163333770516c24266cd1e32991fe616990c
XANkui <1272200579@qq.com>
2018-04-26 14:54:43 +0800

    Unity场景UI灰化效果的实现

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+
+	public GameObject imageGo;
+	public Material imageGrey;
+	private bool isGrey = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			if (imageGo != null && imageGrey != null && isGrey == false) {
+
+				imageGo.GetComponent <Image>().material = imageGrey;
+				isGrey = true;
+			}else if (imageGo != null && imageGrey != null && isGrey == true) {
+
+				imageGo.GetComponent <Image>().material = null;
+				isGrey = false;
+			}
+		}
+	}
+}
